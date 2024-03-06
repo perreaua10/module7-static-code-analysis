7741ec95ad7510485249f0a55c2a50e851600ca7
perreaua10 <perreaua@my.canisius.edu>
2024-03-06 18:59:35 -0500

    Fixed bugs, smells, and vulnerabilities

--- cyb600_module7/bug.py
+++ cyb600_module7/bug.py
@@ -10,11 +10,8 @@ def unlock_the_locks():
     """
     word1 = input("Input the first key: ")
     word2 = input("Input the second key: ")
-    if word1 == "key1":
-        if word2 == "key2":
-            return "Unlocked!"
-    elif word1 == "key1":
-        return "Key 1 Unlocked"
+    if word1 == "key1" and word2 == "key2":
+        return "Unlocked!"
     return "Locked!"
 
 
